53737d41685901581681bfecb62afde8af0f97f7
kou7306 <kouta.1206.y@icloud.com>
2024-06-17 23:30:31 +0900

    複数台同時実行

--- server/test_s.py
+++ server/test_s.py
@@ -5,8 +5,8 @@ from websockets.server import serve
 
 async def echo(websocket):
     async for message in websocket:
-        await websocket.send(message)
-        print(f"Receivedしたよ: {message}")
+       print(f"Receivedしたよ: {message}")
+        
 
 async def main():
     async with serve(echo, "localhost", 8765):
