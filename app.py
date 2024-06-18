d04645e5004aa026d8b4c8974ba76832dae196d0
kou7306 <kouta.1206.y@icloud.com>
2024-06-18 15:03:19 +0900

    動いた

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__, static_folder='static', static_url_path='/')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=1000000)
 CORS(app, supports_credentials=True, responses={r"/*": {"origins": "*"}})
-buffer_size = 6  # バッファサイズ
+buffer_size = 20  # バッファサイズ
 audio_buffer = []  # 音声データのバッファ
 data_count = 0
 
@@ -31,6 +31,7 @@ def handle_connect():
 async def send_data_to_server(datas,url):  
     if not datas:
         return jsonify({'volume': 0, 'frequency': 0})
+    
     # バッファ内のデータの平均値を計算
     volume_avg = np.mean([data['volume'] for data in datas])
     frequency_avg = np.mean([data['frequency'] for data in datas])
@@ -41,23 +42,26 @@ async def send_data_to_server(datas,url):
 
 async def send_data_to_both_servers(audio_buffer):
     url2 = "ws://localhost:8765"
+    url4 = "ws://100.70.4.41:5002"
     # url1 = "wss://apparent-raccoon-close.ngrok-free.app/socket.io"
-    url3= "wss://romantic-rooster-assured.ngrok-free.app/socket.io"
+    # url3= "wss://romantic-rooster-assured.ngrok-free.app/socket.io"
     await asyncio.gather(
         # send_data_to_server(audio_buffer, url1),
         send_data_to_server(audio_buffer, url2),
-        send_data_to_server(audio_buffer, url3)
+        send_data_to_server(audio_buffer, url4)
+        # send_data_to_server(audio_buffer, url3)
     )
 
 
 @socketio.on('audio_data')
 def handle_message(audio_data):
-    print(f'audio_data: {audio_data}')
+    # print(f'audio_data: {audio_data}')
     global audio_buffer, data_count
 
      # バッファに新しいデータを追加
     audio_buffer.append(audio_data)
     data_count += 1
+
     
 
     # バッファサイズを超えたら古いデータを削除
@@ -69,7 +73,7 @@ def handle_message(audio_data):
 
     if data_count >= buffer_size:
         data_count = 0
-        print(audio_buffer)
+        # print(audio_buffer)
         asyncio.run(send_data_to_both_servers(audio_buffer))
 
         
